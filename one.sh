b685604d23cbe350c1b0ba2f7a7ec9786fd6d959
Naveen <naveenavaradi27@gmail.com>
2025-12-14 15:52:28 +0530

    This is a first shell script and trying to add push to GitHub

---
+++ one.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# A simple script to demonstrate a bash shebang
+echo "Hello, World!"
+echo "This script is running with bash."
+echo "Current date and time: $(date)"
+echo "Current working directory: $(pwd)"
+echo "List of files in the current directory:"
+ls -l
